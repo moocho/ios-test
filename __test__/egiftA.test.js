a31d1c525e8694b73f385565407658790443b80a
userlab-luisdeleon <59143067+userlab-luisdeleon@users.noreply.github.com>
2022-05-19 16:15:09 -0600

    update

--- __test__/egiftA.test.js
+++ __test__/egiftA.test.js
@@ -13,7 +13,6 @@ const {
 } = require("../commons/commonsFunctions");
 const api = require("../api/api");
 
-
 /*
 Account A
     - Stripe test card on file
@@ -515,7 +514,298 @@ describe("5. [Account A] Invoice Pop Up — Deal", () => {
     })
 
 })
-// afterAll(async () => {    
-//     let btnBack = await driver.waitForElementById(structure.giftCardActivity.giftCardViewCloseBtn, asserters.isDisplayed, 20000, 100)
-//     await btnBack.click()    
-//     })
+
+describe("6. [Account A] Discount Dropdown — Cannot Afford Reward", () => {
+    
+    test("Reward pop up appears", async () => {
+        let openDiscounts = await driver.elementById(structure.giftCardActivity.giftCardViewOpenDiscountsButton)
+        await openDiscounts.click()        
+        await driver.waitForElementById(structure.giftCardActivity.giftCardViewDiscountBarBody, asserters.isDisplayed, 20000, 100);        
+        let openReward = await driver.elementById(structure.giftCardActivity.giftCardViewDiscountRewardButton)
+        await openReward.click()
+        let rewardView = await driver.waitForElementById(structure.rewardActivity.rewardScreen, asserters.isDisplayed, 20000, 100);        
+        expect(rewardView).not.toBe(null)
+    })
+    test("text appears that says 'YOURS FREE FOR [M] [AMOUNT]'", async () => {
+        let yourFreeLabel = await driver.elementById(structure.rewardActivity.rewardDetailViewYoursFreeForLabel)
+        let yourFreeLabelTxt = await yourFreeLabel.text()        
+        let yourFreeIcon = await driver.elementById(structure.rewardActivity.rewardDetailViewYoursFreeForLabelIcon)
+        let yourFreeMooches = await driver.elementById(structure.rewardActivity.rewardDetailViewYoursFreeForMooches)
+        expect(yourFreeLabelTxt).toContain("YOURS FREE FOR")
+        expect(yourFreeMooches).not.toBe(null)
+        expect(yourFreeIcon).not.toBe(null)
+    })
+    test("button on pop up is disabled / not clickable", async () => {
+        let rewardGetButton = await driver.elementById(structure.rewardActivity.rewardDetailViewGetRewardButton)        
+        let buttonStatus = await rewardGetButton.isEnabled()
+        expect(rewardGetButton).not.toBe(null)
+        expect(buttonStatus).toBe(false)
+    })
+    test("button on pop up says 'EARN [XX] MORE MOOCHES TO GET IT!'", async () => {
+        let rewardGetButtonLabel = await driver.elementById(structure.rewardActivity.rewardDetailViewGetRewardButtonLabel)        
+        let rewardGetButtonLabelTxt = await rewardGetButtonLabel.text()     
+        rewardGetButtonLabelTxt = rewardGetButtonLabelTxt.replace(/[0-9]/g, '')
+        expect(rewardGetButtonLabelTxt).toContain("EARN  MORE MOOCHES TO GET IT!")
+    })
+    test("confirm XX is correct amount based on user's Mooches and price of reward", async () => {
+        let getUserMooches = await api.post(structure.settingsProject.urlGetUserMooches,{"email": userTest[0].email})
+        let userMooches = Number(getUserMooches.data[0].MoochesAvailable)
+        let rewardMoochesRequired = await driver.elementById(structure.rewardActivity.rewardDetailViewYoursFreeForMooches)
+        let rewardMoochesRequiredTxt = await rewardMoochesRequired.text()        
+        let rewardGetButtonLabel = await driver.elementById(structure.rewardActivity.rewardDetailViewGetRewardButtonLabel)        
+        let rewardGetButtonLabelTxt = await rewardGetButtonLabel.text()     
+        let numberFromButton = rewardGetButtonLabelTxt.replace( /[^0-9]/g, '');
+        let result = parseInt(rewardMoochesRequiredTxt) - userMooches
+        let validateResult = (result === parseInt(numberFromButton))
+        expect(validateResult).toBe(true)        
+    })
+
+    test("pop up disappears", async () => {        
+        let closeRewardView = await driver.elementById(structure.rewardActivity.rewardDetailViewGetRewardCloseButton)
+        await closeRewardView.click()
+        let giftCardScreen = await driver.waitForElementById(structure.giftCardActivity.giftCardScreen, asserters.isDisplayed, 20000, 100)
+        expect(giftCardScreen).not.toBe(null)
+    })
+
+    afterAll(async () => {    
+    let btnBack = await driver.waitForElementById(structure.giftCardActivity.giftCardViewCloseBtn, asserters.isDisplayed, 20000, 100)
+    await btnBack.click()    
+    })
+    
+})
+
+describe("7. [Account A] Discount Dropdown — Claim Reward", () => {
+    
+    beforeAll(async () => {    
+        //update mooches
+        await api.post(structure.settingsProject.urlUpdateUserMooches,{"email": userTest[0].email,"value": 0})
+        await api.post(structure.settingsProject.urlUpdateUserMooches,{"email": userTest[0].email,"value": 20})
+        let homeBtn = await driver.elementById(structure.dashboardActivity.btnHome)
+        await homeBtn.click()  
+        await waitForElementUntilDisappear(structure.dashboardActivity.loadingScreen, driver);
+        // open merchant
+        let merchantItem = await driver.elementById(merchantTest[0].name)
+        await merchantItem.click()
+        //await for egift view
+        await driver.waitForElementById(structure.giftCardActivity.giftCardScreen, asserters.isDisplayed, 60000, 100);
+    })
+
+    test("Reward pop up appears", async () => {
+        let openDiscounts = await driver.elementById(structure.giftCardActivity.giftCardViewOpenDiscountsButton)
+        await openDiscounts.click()        
+        await driver.waitForElementById(structure.giftCardActivity.giftCardViewDiscountBarBody, asserters.isDisplayed, 20000, 100);        
+        let openReward = await driver.elementById(structure.giftCardActivity.giftCardViewDiscountRewardButton)
+        await openReward.click()
+        let rewardView = await driver.waitForElementById(structure.rewardActivity.rewardScreen, asserters.isDisplayed, 20000, 100);        
+        expect(rewardView).not.toBe(null)
+    })
+    test("text appears that says 'YOURS FREE FOR [M] [AMOUNT]'", async () => {
+        let yourFreeLabel = await driver.elementById(structure.rewardActivity.rewardDetailViewYoursFreeForLabel)
+        let yourFreeLabelTxt = await yourFreeLabel.text()        
+        let yourFreeIcon = await driver.elementById(structure.rewardActivity.rewardDetailViewYoursFreeForLabelIcon)
+        let yourFreeMooches = await driver.elementById(structure.rewardActivity.rewardDetailViewYoursFreeForMooches)
+        expect(yourFreeLabelTxt).toContain("YOURS FREE FOR")
+        expect(yourFreeMooches).not.toBe(null)
+        expect(yourFreeIcon).not.toBe(null)
+    })
+    test("button on pop up is enabled", async () => {
+        let rewardGetButton = await driver.elementById(structure.rewardActivity.rewardDetailViewGetRewardButton)        
+        let buttonStatus = await rewardGetButton.isEnabled()
+        expect(rewardGetButton).not.toBe(null)
+        expect(buttonStatus).toBe(true)
+    })
+    test("button on pop up says 'GET IT NOW!'", async () => {
+        let rewardGetButtonLabel = await driver.elementById(structure.rewardActivity.rewardDetailViewGetRewardButtonLabel)        
+        let rewardGetButtonLabelTxt = await rewardGetButtonLabel.text()             
+        expect(rewardGetButtonLabelTxt).toContain("GET IT NOW!")
+    })
+    test("text on button changes to 'GETTING IT!...'", async () => {
+        let rewardGetButton = await driver.elementById(structure.rewardActivity.rewardDetailViewGetRewardButton)        
+        await rewardGetButton.click()
+        let rewardGetButtonLabel = await driver.elementById(structure.rewardActivity.rewardDetailViewGetRewardButtonLabel)        
+        let rewardGetButtonLabelTxt = await rewardGetButtonLabel.text()             
+        expect(rewardGetButtonLabelTxt).toContain("GETTING IT!...")
+    })
+    test("text on button changes to 'USE IT NOW!'", async () => {        
+        let rewardGetButtonLabel = await driver.elementById(structure.rewardActivity.rewardDetailViewGetRewardButtonLabel)        
+        let rewardGetButtonLabelTxt = await rewardGetButtonLabel.text()             
+        expect(rewardGetButtonLabelTxt).toContain("USE IT NOW!")
+    })
+    test("Tap 'USE IT NOW!'", async () => {        
+        let rewardGetButton = await driver.elementById(structure.rewardActivity.rewardDetailViewGetRewardButton)        
+        await rewardGetButton.click()
+        let giftCardScreen = await driver.waitForElementById(structure.giftCardActivity.giftCardScreen, asserters.isDisplayed, 20000, 100)
+        expect(giftCardScreen).not.toBe(null)        
+    })
+    test("pop up collapses", async () => {        
+        let rewardPopUp = await findIosElement(structure.rewardActivity.rewardScreen,driver)        
+        expect(rewardPopUp).toBe(false)        
+    })
+    test("discount dropdown collapses", async () => {        
+        let discountBody = await findIosElement(structure.giftCardActivity.giftCardViewDiscountBarBody,driver)        
+        expect(discountBody).toBe(false)        
+    })
+    test("button on Reward now says 'SEE DETAILS'", async () => {        
+        let openDiscounts = await driver.elementById(structure.giftCardActivity.giftCardViewOpenDiscountsButton)
+        await openDiscounts.click()        
+        await driver.waitForElementById(structure.giftCardActivity.giftCardViewDiscountBarBody, asserters.isDisplayed, 20000, 100); 
+        let rewardDiscountButton = await driver.elementById(structure.giftCardActivity.giftCardViewDiscountRewardButtonLabel)
+        let rewardDiscountButtonTxt = await rewardDiscountButton.text()
+        expect(rewardDiscountButtonTxt).toBe("SEE DETAILS")
+    })    
+})
+
+describe("8. [Account A] Invoice Pop Up — Reward", () => {
+    
+    test("'You'll pay...' with correct amount to be charged to card and (i) button appears", async () => {
+        let number4 = await driver.elementById(structure.giftCardActivity.giftCardCardViewCalculator.giftCardCardViewCalculatorNumberFour)
+        await number4.click()    
+        let numberDoubleZero = await driver.elementById(structure.giftCardActivity.giftCardCardViewCalculator.giftCardCardViewCalculatorNumberDoubleZero)
+        await numberDoubleZero.click()
+        await numberDoubleZero.click()         
+        await waitForElementUntilDisappear(structure.giftCardActivity.giftCardViewYouWillPayLoading,driver)
+        let youwillPayLabel = await driver.elementById(structure.giftCardActivity.giftCardViewYouWillPayLabel)
+        let youWillPayButton = await driver.elementById(structure.giftCardActivity.giftCardViewYouWillPayButton)        
+        expect(youwillPayLabel).not.toBe(null)
+        expect(youWillPayButton).not.toBe(null)        
+    })
+
+    test("pop up appears", async () => {
+        let youWillPayButton = await driver.elementById(structure.giftCardActivity.giftCardViewYouWillPayButton)
+        youWillPayButton.click()        
+        let payDetailPopUp = await driver.waitForElementById(structure.giftCardActivity.giftCardViewPayDetailView, asserters.isDisplayed, 20000, 100)
+        expect(payDetailPopUp).not.toBe(null)
+    })
+
+    test("First line is 'Total bill:'", async () => {
+    let totalBill = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewTotalBill)        
+    expect(totalBill).not.toBe(null)
+    })
+
+    test("Correct amount entered", async () => {
+    let totalBill = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewTotalBill)
+    let totalBIllTxt = await totalBill.text()                
+    let amountCalculator = await driver.elementById(structure.giftCardActivity.giftCardViewCalculatorAmountLabel)
+    let amountCalculatorTxt = await amountCalculator.text()                
+    let validateAmount = (totalBIllTxt === amountCalculatorTxt)
+    expect(validateAmount).toBe(true)
+    })
+
+    //REWARD
+    test("Next line is 'Minus Reward discount:'", async () => {
+    let minusDiscount = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewReward)        
+    expect(minusDiscount).not.toBe(null)
+    })
+    test("Correct amount is displayed with minus sign in front of it", async () => {
+    let minusDiscountAmount = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewRewardValue)        
+    let minusDiscountAmountTxt = await minusDiscountAmount.text()
+    let validateAmount = minusDiscountAmountTxt.includes('-')        
+    expect(validateAmount).toBe(true)
+    })
+    test("Correct Headline of Reward discount is displayed in gold text underneath Reward discount line", async () => {        
+    let minusDiscountName = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewRewardName)        
+    let minusDiscountAmountTxt = await minusDiscountName.text()                
+    expect(minusDiscountAmountTxt).not.toBe("Applicable Discount!")
+    })
+
+    //ANYWHERE CREDIT
+    test("Next line is 'Minus Anywhere Credit:'", async () => {        
+    //thisGonaFail
+    let anyWhereCredit = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewAnyWhereCredit2)        
+    expect(anyWhereCredit).not.toBe(null)
+    })
+    test("Correct amount is displayed with minus sign in front of it", async () => {        
+    let minusDiscountanyWhereCredit = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewAnyWhereCreditValue)        
+    let minusDiscountanyWhereCreditTxt = await minusDiscountanyWhereCredit.text()        
+    let validateAmount = minusDiscountanyWhereCreditTxt.includes('-')        
+    expect(validateAmount).toBe(true)
+    })
+
+    //MERCHANT CREDIT
+    test("Next line is 'Minus [merchant name] Credit:'", async () => {        
+    //thisGonaFail
+    let merchantCredit = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewMerchant2)        
+    expect(merchantCredit).not.toBe(null)
+    })
+    test("Correct amount is displayed with minus sign in front of it", async () => {        
+    let minusDiscountMerchantCredit = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewMerchantValue)        
+    let minusDiscountMerchantCreditTxt = await minusDiscountMerchantCredit.text()              
+    let validateAmount = minusDiscountMerchantCreditTxt.includes('-')        
+    expect(validateAmount).toBe(true)
+    })
+
+    //MOOCHOCASH CREDIT
+    test("Next line is 'Minus Moocho Cash:'", async () => {        
+    //thisGonaFail
+    let moochoCashCredit = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewMoochoCash2)        
+    expect(moochoCashCredit).not.toBe(null)
+    })
+    test("Correct amount is displayed with minus sign in front of it", async () => {        
+    let discountAmount = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewMoochoCashValue)        
+    let discountAmountTxt = await discountAmount.text()            
+    let validateAmount = discountAmountTxt.includes('-')        
+    expect(validateAmount).toBe(true)
+    })
+
+    //BONUSCASH CREDIT
+    test("Next line is 'Minus Bonus Cash:'", async () => {        
+    //thisGonaFail
+    let bonusCashCredit = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewMoochoBonus2)        
+    expect(bonusCashCredit).not.toBe(null)
+    })
+    test("Correct amount is displayed with minus sign in front of it", async () => {        
+    let discountAmount = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewMoochoBonusValue)        
+    let discountAmountTxt = await discountAmount.text()                   
+    let validateAmount = discountAmountTxt.includes('-')        
+    expect(validateAmount).toBe(true)
+    })
+
+    //CARD ON FILE
+    test("Next line is 'Card on file:'", async () => {                
+    let cardOnFile = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewCardOnFileLabel)        
+    expect(cardOnFile).not.toBe(null)
+    })
+    test("Correct amount to be charged on card is displayed (total bill minus value of all discounts)", async () => {        
+    let rewardDiscount = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewRewardValue)
+    let merchantDiscount = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewMerchantValue)
+    let moochoCashDiscount = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewMoochoCashValue)
+    let bonusCashDiscount = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewMoochoBonusValue)
+    let anyWhereDiscount = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewAnyWhereCreditValue)
+    let cardOnFileValue = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewCardOnFileValue)
+    let totalBill = await driver.elementById(structure.giftCardActivity.giftCardViewPayDetailViewTotalBill)
+
+    let totalBillTxt = await totalBill.text()
+    let cardOnFileValueTxt = await cardOnFileValue.text()
+    let rewardDiscountTxt = await rewardDiscount.text()
+    let merchantDiscountTxt = await merchantDiscount.text()
+    let moochoCashDiscountTxt = await moochoCashDiscount.text()
+    let bonusCashDiscountTxt = await bonusCashDiscount.text()
+    let anyWhereDiscountTxt = await anyWhereDiscount.text()
+
+    totalBillTxt = totalBillTxt.replace('$','')
+    cardOnFileValueTxt = cardOnFileValueTxt.replace('$','')
+    rewardDiscountTxt = rewardDiscountTxt.replace('-$','')
+    merchantDiscountTxt = merchantDiscountTxt.replace('-$','')
+    moochoCashDiscountTxt = moochoCashDiscountTxt.replace('-$','')
+    bonusCashDiscountTxt = bonusCashDiscountTxt.replace('-$','')
+    anyWhereDiscountTxt = anyWhereDiscountTxt.replace('-$','')
+
+    let sumDiscount = ( parseFloat(rewardDiscountTxt) + 
+            parseFloat(merchantDiscountTxt) + 
+            parseFloat(moochoCashDiscountTxt) + 
+            parseFloat(bonusCashDiscountTxt) + 
+            parseFloat(anyWhereDiscountTxt))
+
+    let billMinusDiscounts = (parseFloat(totalBillTxt) - sumDiscount)
+    let cardOnFile = parseFloat(cardOnFileValueTxt)
+            
+    expect(billMinusDiscounts).toBe(cardOnFile)
+
+    })
+
+
+afterAll(async () => {    
+    let btnBack = await driver.waitForElementById(structure.giftCardActivity.giftCardViewCloseBtn, asserters.isDisplayed, 20000, 100)
+    await btnBack.click()    
+    })
+})
